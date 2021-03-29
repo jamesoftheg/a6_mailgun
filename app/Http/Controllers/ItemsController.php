4c27282d51b8c103573dddec97dceeaeb3ba8b87
jamesoftheg <gelfandjames@gmail.com>
2021-03-29 00:16:36 -0400

    Updated

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ItemsController extends Controller
 {
-    //
+    public function index() {
+        $title = 'Lab 6 - Mailgun';
+        // This is one way to pass parameters.
+        // return view('pages.index', compact('title'));
+        // This is a good way to pass in arrays.
+        return view('pages.index')->with('title', $title);
+    }
 }
