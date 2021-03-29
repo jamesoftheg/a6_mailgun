f20d4833f179e0ac6d142f741fdb49065a130936
jamesoftheg <gelfandjames@gmail.com>
2021-03-29 01:38:14 -0400

    Updated

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+<!-- This specifies the layout we want to use -->
+@extends('layouts.app')
+
+<!-- If we want this to go into our content we have to wrap in section -->
+@section('content')
+    <h1>Home</h1>
+    <p>This is the index page.</p>
+@endsection
