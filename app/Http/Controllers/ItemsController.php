3032cb77595f6acc52d737d064c23b5a9f11f23b
jamesoftheg <gelfandjames@gmail.com>
2021-04-01 00:30:27 -0400

    Updated

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -15,8 +15,8 @@ class ItemsController extends Controller
      */
     public function index()
     {
-        //$items = Item::all();
-        return view('items.index');
+        $items = Item::all();
+        return view('items.index')->with('items', $items);
     }
 
     /**
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -8,6 +8,42 @@
         <p>Goods and services.</p>
 
         <h1>Items</h1>
-        
+        @if(count($items) > 0)
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">Item Name:</th>
+                    <th scope="col">Description:</th>
+                    <th scope="col">Price:</th>
+                    <th scope="col">Quantity:</th>
+                    <th scope="col">Add to Cart:</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach($items as $key => $item)
+                        <tr>
+                            <td>{{$item->name}}</td>
+                            <td>{{$item->description}}</td>
+                            <td>{{$item->price}}</td>
+                            <form action="/items" method="post">
+                            <td>                    
+                                @csrf
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="quantity"  name="quantity">
+                                </div>
+                            </td>
+                            <td>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </td>
+                            </form>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>No items found.</p>
+        @endif
 
+    </div>
+    
 @endsection
