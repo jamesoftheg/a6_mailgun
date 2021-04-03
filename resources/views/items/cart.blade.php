fd4a56ceded340e0aee6f50dd57abfc2de50d2f1
jamesoftheg <gelfandjames@gmail.com>
2021-04-03 15:58:03 -0400

    Updated

--- resources/views/items/cart.blade.php
+++ resources/views/items/cart.blade.php
@@ -7,7 +7,46 @@
 <div class="container">
         
     <h1>Shopping Cart</h1>
+    <div class="container">
+        <div class="hotelcard">
+        @if(session('cart'))
+            <table class="table" id="cart">
+                <thead>
+                <tr>
+                    <th scope="col">Item:</th>
+                    <th scope="col">Item Price:</th>
+                    <th scope="col">Quantity:</th>
+                    <th scope="col">Total Price:</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach(session('cart') as $key => $item)
+                        <tr>
+                            <?php $totalprice = $item->price * $item->quantity ?>
+                            <td>{{$item->name}}</td>
+                            <td>{{$item->price}}</td>
+                            <td>{{$item->quantity}}</td>
+                            <td><?php $totalprice ?></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>No items found.</p>
+        @endif
+        </div>
+    </div>
 
 </div>
+
+
+
+
+
+
+
+
+
+
 
 @endsection
