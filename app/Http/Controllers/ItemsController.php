820752933d1f967e685ac64143ee4a5851eb9d7f
jamesoftheg <gelfandjames@gmail.com>
2021-04-04 21:48:57 -0400

    Updated

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Item;
 use Illuminate\Support\Facades\Validator;
+use App\Mail\OrdersShipped;
 
 class ItemsController extends Controller
 {
