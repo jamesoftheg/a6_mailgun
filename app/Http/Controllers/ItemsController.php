3e96ff75c7ad8c64613aff2b6bb3f209f1b9e233
jamesoftheg <gelfandjames@gmail.com>
2021-04-04 22:32:47 -0400

    Updated

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -21,72 +21,6 @@ class ItemsController extends Controller
         return view('items.index')->with('items', $items);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        return Item::find($id);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
     public function add(Request $request, $id) {
 
         $item = Item::find($id);
@@ -147,12 +81,6 @@ class ItemsController extends Controller
 
         Mail::to('gelfandjames@gmail.com')
         ->send($receipt);
-
-        /*
-        Mail::to('gelfandjames@gmail.com')
-        ->cc($moreUsers)
-        ->send($receipt));
-        */
 
         return $receipt->render();
     }
--- resources/views/emails/orders/shipped.blade.php
+++ resources/views/emails/orders/shipped.blade.php
@@ -4,39 +4,46 @@
 The body of your message.
 
 <div class="container">
-    <div class="hotelcard">
+<div class="hotelcard">
 
-    <?php $rowTotal = 0; ?>
-    <?php $total = 0; ?>
+<?php $rowTotal = 0; ?>
+<?php $total = 0; ?>
 
-    @if(session()->has('cart'))
-       <p>Cart found.</p>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">Item Name:</th>
-                <th scope="col">Price:</th>
-                <th scope="col">Quantity:</th>
-                <th scope="col">Total:</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach(session('cart') as $id => $item)
-                <p hidden>{{$total += $item['quantity'] * $item['price']}}</p>
-                <tr>
-                    <td>{{$item['name']}}</td>
-                    <td>{{$item['price']}}</td>
-                    <td>{{$item['quantity']}}</td>
-                    <td>{{$item['quantity'] * $item['price']}}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-        <p><?php $total ?></p>
-    @else
-        <p>Cart empty.</p>
-    @endif
-    </div>
+@component('mail::table')
+| Laravel       | Table         | Example  |
+| ------------- |:-------------:| --------:|
+| Col 2 is      | Centered      | $10      |
+| Col 3 is      | Right-Aligned | $20      |
+@endcomponent
+
+@if(session()->has('cart'))
+<p>Cart found.</p>
+<table class="table">
+<thead>
+<tr>
+<th scope="col">Item Name:</th>
+<th scope="col">Price:</th>
+<th scope="col">Quantity:</th>
+<th scope="col">Total:</th>
+</tr>
+</thead>
+<tbody>
+@foreach(session('cart') as $id => $item)
+<p hidden>{{$total += $item['quantity'] * $item['price']}}</p>
+<tr>
+<td>{{$item['name']}}</td>
+<td>{{$item['price']}}</td>
+<td>{{$item['quantity']}}</td>
+<td>{{$item['quantity'] * $item['price']}}</td>
+</tr>
+@endforeach
+</tbody>
+</table>
+<p><?php $total ?></p>
+@else
+<p>Cart empty.</p>
+@endif
+</div>
 </div>
 
 @component('mail::button', ['url' => ''])
